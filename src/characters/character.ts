fee44e853545a522b678a4be4291158a0532b71d
jacob.shuman <jacob.shuman7@gmail.com>
2019-01-19 23:25:47 -0500

    added character gender enum

--- src/characters/character.ts
+++ src/characters/character.ts
@@ -4,6 +4,7 @@ export interface CharacterInterface {
   // General information
   readonly name?: string;
   readonly age?: number;
+  readonly gender?: CharacterGender;
 
   // Stats
   readonly health?: number;
@@ -11,14 +12,22 @@ export interface CharacterInterface {
 
 export interface CharacterOptionsInterface extends CharacterInterface {}
 
+export enum CharacterGender {
+  MALE,
+  FEMALE
+}
+
 export class Character implements CharacterInterface {
   // Interface properties
   readonly name: string;
+  readonly gender: CharacterGender;
   readonly age: number;
   readonly health: number;
 
   constructor(options: CharacterOptionsInterface) {
     this.name = options.name || DEFAULTS.DEFAULT_NAME;
+    this.gender = options.gender || DEFAULTS.DEFAULT_GENDER;
+
     this.age = options.age || DEFAULTS.DEFAULT_AGE;
     this.health = options.health || DEFAULTS.DEFAULT_HEALTH;
   }
--- src/characters/defaults.ts
+++ src/characters/defaults.ts
@@ -1,4 +1,7 @@
+import { CharacterGender } from "./character";
+
 export const DEFAULT_NAME: string = "";
+export const DEFAULT_GENDER: CharacterGender = CharacterGender.MALE;
 export const DEFAULT_AGE: number = 0;
 
 export const DEFAULT_HEALTH: number = 0;
